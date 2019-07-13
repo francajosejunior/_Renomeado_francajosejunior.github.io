4541ee75e96ab7d3ff27c773193e2a4647908deb
Jose Joaquim Junior <jose.junior@partnergroup.com.br>
2019-07-13 00:50:22 -0300

    << JJ >>

--- image-perspective/sw.js
+++ image-perspective/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.954a96652a332fa336a393b9f7925d87.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.0814312ff1ee7c7254bb763bd54bbe87.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest)
 
