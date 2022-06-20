133f4a963cbcba35f53c48f23632b6f75f8bfe47
josejoaquim <jjr_frank@hotmail.com>
2022-06-20 16:21:33 -0300

    deploy

--- apps/stopwatch/deploy.sh
+++ apps/stopwatch/deploy.sh
@@ -1,6 +1,11 @@
 yarn build
 cd ../../
-rm !(apps)
+ls > lista.txt
+echo "$(tail -n +2 lista.txt)" > lista.txt
+xargs rm -v < lista.txt
+rm -v < lista.txt
+rm -rf static
+rm -rf lista.txt
 cd ./apps/stopwatch/build
 mv ./* ../../../
 git add .
